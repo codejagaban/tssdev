0a5aba4cb7b1a54c748051901e97606508df9339
codekyd <okpusjamin@gmail.com>
2020-12-21 13:38:13 +0100

    chore: add blog to homepage

--- src/components/Blog/BlogItem.js
+++ src/components/Blog/BlogItem.js
@@ -1,34 +1,33 @@
 import React from "react"
 import styled from "styled-components"
-import image from "../../images/dish1.png"
-import arrowRight from "../../images/arrow-right-white.svg"
+import image from "../../images/blog-image.png"
 
 const BlogItem = () => {
   return (
     <ItemWrapper>
       <img src={image} alt="Food" />
       <div>
-      <h6>Food</h6>
-      <h5>Fresh fish and onions</h5>
+      <h6>Food and life</h6>
+      <h5>Succed in hotel business</h5>
       <p>
         Fresh food directly from our restaurant ready coocked for you and you familly
       </p>
       </div>
-      <button>
-        <img src={arrowRight} alt=""/>
-      </button>
+
     </ItemWrapper>
   )
 }
 const ItemWrapper = styled.div`
   background-color: #EEEEEE;
-  height: 190px;
   margin: 10px;
-  display: flex;
-  flex: 0 0 48%;
+  flex: 0 0 31.3333%;
+  img{
+    object-fit: cover;
+    width: 100%;
+  }
   div {
-    margin-left: 5px;
     padding: 30px;
+    text-align: center;
     h5 {
       font-size: 18px;
       font-weight: 600;
@@ -37,14 +36,6 @@ const ItemWrapper = styled.div`
     p {
       opacity: 0.5
     }
-  }
-  button {
-    padding: 20px;
-    align-self: flex-end;
-    background-color: #5863F8;
-    outline: none;
-    cursor: pointer;
-    border: 1px solid #5863F8;
   }
   `
 export default BlogItem
